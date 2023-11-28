aef3ecd5a65380d78abdba313c6607771582172f
9442villars <9442villars@WVD-COB-DSK-57>
2023-11-28 14:39:08 -0600

    Add project files.

---
+++ Class28th/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Insy
+{
+
+    public class Program
+    {
+
+        static void Main(string[])
+        {
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
